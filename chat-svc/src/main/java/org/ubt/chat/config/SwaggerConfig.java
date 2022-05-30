b837f6944f63b3953b7304ffd6bebac02649a187
uranik12 <uh48222@ubt-uni.net>
2022-05-30 23:26:15 +0200

    Add endpoint documentation - Swagger

---
+++ chat-svc/src/main/java/org/ubt/chat/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package org.ubt.chat.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI springOpenAPI() {
+        return new OpenAPI()
+                .info(getApiInfo());
+    }
+
+    private Info getApiInfo() {
+        return new Info()
+                .title("Chat API")
+                .description("Documentation of API Endpoints")
+                .version("v1.0")
+                .license(null)
+                .termsOfService("Terms of service");
+    }
+
+}
