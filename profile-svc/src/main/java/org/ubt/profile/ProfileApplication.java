bdce6990625d61b9b68114e74a2233a38c56916e
ilirianthaci18 <ilirianthaqi@live.com>
2022-06-04 00:53:39 +0200

    Add service discovery

--- profile-svc/src/main/java/org/ubt/profile/ProfileApplication.java
+++ profile-svc/src/main/java/org/ubt/profile/ProfileApplication.java
@@ -2,8 +2,10 @@ package org.ubt.profile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProfileApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProfileApplication.class,args);
